4ba81c8bb6fc184220a903a288d40a7cf866ff0d
Paz Lazar <pazer@gmx.com>
2017-03-10 21:13:01 +0200

    production

---
+++ CINS-Client/ecosystem.config1.js
@@ -0,0 +1,43 @@
+let cwd     =   process.env.NODE_ENV === 'Development' ? '/var/www/html/CINS/CINS-Server-Express/App/Cronjobs' : '/var/www/CINS/CINS-Server-Express/App/Cronjobs';
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+      {
+          name      : "Client",
+          script    : "app.js",
+          watch     : true,
+          exec_mode: 'cluster'
+      }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : "node",
+      host : "212.83.163.1",
+      ref  : "origin/master",
+      repo : "git@github.com:repo.git",
+      path : "/var/www/production",
+      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.json --env production"
+    },
+    dev : {
+      user : "node",
+      host : "212.83.163.1",
+      ref  : "origin/master",
+      repo : "git@github.com:repo.git",
+      path : "/var/www/development",
+      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.json --env dev",
+      env  : {
+        NODE_ENV: "dev"
+      }
+    }
+  }
+}
