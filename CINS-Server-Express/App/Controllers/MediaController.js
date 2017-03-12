ce0080f1c088b932bef60d905b505a6f330af38c
Paz Lazar <pazer@gmx.com>
2017-03-12 14:41:04 +0200

    rss

--- CINS-Server-Express/App/Controllers/MediaController.js
+++ CINS-Server-Express/App/Controllers/MediaController.js
@@ -115,8 +115,8 @@ class MediaController {
                     feed.item({
                         title: media.Title,
                         description: `
-                        From: ${media.Source} \n
-                        At: ${media.PublishedAt} \n
+                        From: ${media.Source} <br />
+                        At: ${media.PublishedAt} <br />
                         ${media.Description || media.Title}
                         `,
                         url: media.Url,
